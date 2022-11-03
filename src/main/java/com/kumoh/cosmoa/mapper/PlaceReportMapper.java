397ee4c2bf98cd284ad5bcefc1d79463c8e535c2
km7865 <km78655@gmail.com>
2022-11-03 14:17:09 +0900

    장소신고 기능 구현 장소신고 등록 구현 장소신고 삭제 구현 장소신고 목록 조회 구현

---
+++ src/main/java/com/kumoh/cosmoa/mapper/PlaceReportMapper.java
@@ -0,0 +1,15 @@
+package com.kumoh.cosmoa.mapper;
+
+import com.kumoh.cosmoa.dto.PlaceReportDTO;
+import com.kumoh.cosmoa.dto.PlaceReportResponseDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface PlaceReportMapper {
+    List<PlaceReportResponseDTO> findAll();
+    int insertPlaceReport(@Param("param")PlaceReportDTO dto);
+    int deletePlaceReport(@Param("reportId") int reportId);
+}
---
+++ src/main/java/com/kumoh/cosmoa/service/PlaceReportService.java
@@ -0,0 +1,34 @@
+package com.kumoh.cosmoa.service;
+
+
+import com.kumoh.cosmoa.dto.PlaceReportDTO;
+import com.kumoh.cosmoa.dto.PlaceReportResponseDTO;
+import com.kumoh.cosmoa.mapper.PlaceReportMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class PlaceReportService {
+    private final PlaceReportMapper placeReportMapper;
+
+    public PlaceReportService(PlaceReportMapper placeReportMapper) {
+        this.placeReportMapper = placeReportMapper;
+    }
+
+    public PlaceReportDTO insert(PlaceReportDTO dto) {
+        if (placeReportMapper.insertPlaceReport(dto) == 0) throw new RuntimeException("Insert PlaceReport failed. try again.");
+        return null;
+    }
+
+    public List<PlaceReportResponseDTO> findAll() {
+        //log.info("PlaceReportResponseDTO : {}", placeReportMapper.findAll().get(0));
+        return placeReportMapper.findAll();
+    }
+
+    public boolean delete(int reportId) {
+        return placeReportMapper.deletePlaceReport(reportId) == 1;
+    }
+}
