b77b6651b9d38e23b157adfefa5cdcf5f0eb5c61
km7865 <km78655@gmail.com>
2022-11-17 12:11:55 +0900

    UserController 수정 @RequestBody 명시 유저 정보 수정 시 패스워드를 암호화하는 로직 추가

--- src/main/java/com/kumoh/cosmoa/controller/UserController.java
+++ src/main/java/com/kumoh/cosmoa/controller/UserController.java
@@ -21,95 +21,97 @@ import java.util.List;
 @RequestMapping("/user")
 @Slf4j
 public class UserController {
-    private UserService userService;
+	private UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
 
-    @GetMapping("")
-    public ResponseEntity<?> getUserList() {
-        List<UserDTO> dtos = userService.findAll();
-        ResponseDTO<List<UserDTO>> response = ResponseDTO.<List<UserDTO>>builder().data(dtos).build();
-     
-        return ResponseEntity.ok().body(response);
-    }
-    
-    @PostMapping("")
-    public ResponseEntity<?> signUp(@RequestBody UserDTO userDto) {
-    	try {
-    		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    		userDto.setPassword(encoder.encode(userDto.getPassword()));
-    		int result = userService.signUp(userDto);
-    		if(result==1)
-    		{
-    			return ResponseEntity.ok().body(ResponseDTO.builder().data("유저 등록 성공").build());
-    		}
-    		else
-    		{
-    			return ResponseEntity.ok().body(ResponseDTO.builder().data("유저 등록 실패").build());
-    		}    		
-    	} catch (Exception e) {
-    		return ResponseEntity.badRequest().body(ResponseDTO.builder().error(e.getMessage()).build());
-    	}
-    }
-    
-    @PutMapping("/{email}")
-    public ResponseEntity<?> updateUser(@PathVariable String email, UserDTO userDto) {
-    	try {
-    		userDto.setEmail(email);
-    		int result = userService.updateUser(userDto);
-    		if(result==1)
-    		{
-    			return ResponseEntity.ok().body(ResponseDTO.builder().data("유저 정보 수정 성공").build());
-    		}
-    		else
-    		{
-    			return ResponseEntity.ok().body(ResponseDTO.builder().data("유저 정보 수정 실패").build());
-    		}	
-    	} catch (Exception e) {
-    		return ResponseEntity.badRequest().body(ResponseDTO.builder().error(e.getMessage()).build());
-    	}
-    }
-    
-    @DeleteMapping("/{email}")
-    public ResponseEntity<?> deleteUserByEmail(@PathVariable String email) {
-    	try {
-    		int result = userService.deleteUserByEmail(email);
-    		
-    		if(result==1)
-    		{
-    			return ResponseEntity.ok().body(ResponseDTO.builder().data(email + " 유저 삭제 완료").build());
-    		}
-    		else
-    		{
-    			return ResponseEntity.ok().body(ResponseDTO.builder().data("유저 삭제 실패").build());
-    		}
-    	} catch (Exception e) {
-    		return ResponseEntity.badRequest().body(ResponseDTO.builder().error(e.getMessage()).build());
-    	}
-    }
-    
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserDTO dto){
+	@GetMapping("")
+	public ResponseEntity<?> getUserList() {
+		List<UserDTO> dtos = userService.findAll();
+		ResponseDTO<List<UserDTO>> response = ResponseDTO.<List<UserDTO>>builder().data(dtos).build();
+
+		return ResponseEntity.ok().body(response);
+	}
+
+	@PostMapping("")
+	public ResponseEntity<?> signUp(@RequestBody UserDTO userDto) {
+		try {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			userDto.setPassword(encoder.encode(userDto.getPassword()));
+			int result = userService.signUp(userDto);
+			if(result==1)
+			{
+				return ResponseEntity.ok().body(ResponseDTO.builder().data("유저 등록 성공").build());
+			}
+			else
+			{
+				return ResponseEntity.ok().body(ResponseDTO.builder().data("유저 등록 실패").build());
+			}
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(ResponseDTO.builder().error(e.getMessage()).build());
+		}
+	}
+
+	@PutMapping("/{email}")
+	public ResponseEntity<?> updateUser(@PathVariable String email, @RequestBody UserDTO userDto) {
+		try {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			userDto.setEmail(email);
+			userDto.setPassword(encoder.encode(userDto.getPassword()));
+			int result = userService.updateUser(userDto);
+			if(result==1)
+			{
+				return ResponseEntity.ok().body(ResponseDTO.builder().data("유저 정보 수정 성공").build());
+			}
+			else
+			{
+				return ResponseEntity.ok().body(ResponseDTO.builder().data("유저 정보 수정 실패").build());
+			}
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(ResponseDTO.builder().error(e.getMessage()).build());
+		}
+	}
+
+	@DeleteMapping("/{email}")
+	public ResponseEntity<?> deleteUserByEmail(@PathVariable String email) {
+		try {
+			int result = userService.deleteUserByEmail(email);
+
+			if(result==1)
+			{
+				return ResponseEntity.ok().body(ResponseDTO.builder().data(email + " 유저 삭제 완료").build());
+			}
+			else
+			{
+				return ResponseEntity.ok().body(ResponseDTO.builder().data("유저 삭제 실패").build());
+			}
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(ResponseDTO.builder().error(e.getMessage()).build());
+		}
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestBody UserDTO dto){
 		log.info("user dto : {}", dto != null);
-    	try {
+		try {
 
-    		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    		UserDTO originalUser = userService.findUserByEmail(dto.getEmail());
-    		if(encoder.matches(dto.getPassword(), originalUser.getPassword()))
-    		{
-    			return ResponseEntity.ok().body(ResponseDTO.builder().data(originalUser).build());
-    		}
-    		else return ResponseEntity.ok().body(ResponseDTO.builder().error(("비밀번호 불일치")).build());
-    		
-    	} catch (Exception e) {
-    		return ResponseEntity.badRequest().body(ResponseDTO.builder().error(e.getMessage()).build());
-    	}
-    }
-    @GetMapping("/logout")
-    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
-        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
-        return "redirect:/user/login";
-      }
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			UserDTO originalUser = userService.findUserByEmail(dto.getEmail());
+			if(encoder.matches(dto.getPassword(), originalUser.getPassword()))
+			{
+				return ResponseEntity.ok().body(ResponseDTO.builder().data(originalUser).build());
+			}
+			else return ResponseEntity.ok().body(ResponseDTO.builder().error(("비밀번호 불일치")).build());
+
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(ResponseDTO.builder().error(e.getMessage()).build());
+		}
+	}
+	@GetMapping("/logout")
+	public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+		new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+		return "redirect:/user/login";
+	}
 }
