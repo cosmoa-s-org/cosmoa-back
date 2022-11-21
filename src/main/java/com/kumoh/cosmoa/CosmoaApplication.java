d7257c3070503ae2bea0aaabf1d29eb51fd547c4
km7865 <km78655@gmail.com>
2022-11-21 09:21:55 +0900

    war 생성을 위한 소스코드 추가

--- src/main/java/com/kumoh/cosmoa/CosmoaApplication.java
+++ src/main/java/com/kumoh/cosmoa/CosmoaApplication.java
@@ -2,9 +2,15 @@ package com.kumoh.cosmoa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CosmoaApplication {
+public class CosmoaApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CosmoaApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CosmoaApplication.class, args);
 	}
